ff8cf889d7686fbca05a666a1339fe95a5ba6461
qwuzyaa <nassenova@inbox.ru>
2026-03-23 22:14:04 +0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,17 +1,18 @@
 CREATE TABLE users (
-	id INTEGER PRIMARY KEY, 
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
-    created_time DATETIME DEFAULT CURRENT_TIMESTAMP
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT UNIQUE NOT NULL,
+    created_time TEXT NOT NULL
 );
 
 CREATE TABLE tasks (
-	id INTEGER PRIMARY KEY, 
-    user_id INTEGER,
-    name TEXT NOT NULL, 
-    description TEXT, 
-    created_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    time_limit DATETIME,
-    status BOOLEAN DEFAULT 0,
+	user_id INTEGER NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    description TEXT,
+    status INTEGER DEFAULT 0, 
+    limit_time TEXT,
+    created_time TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
