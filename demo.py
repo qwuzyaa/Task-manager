ba1a46f7cecfdff277d37b614bf92e7974bb9b92
qwuzyaa <nassenova@inbox.ru>
2026-04-21 02:28:19 +0300

    Delete demo.py

--- demo.py
+++
@@ -1,8 +0,0 @@
-from application.functions import *
-from application.schemes import *
-
-#create_user('ADMIN', 'admin_1', 12345678)
-user = get_user_username("admin_1")
-#print(get_user_name("ADMIN"))
-print(get_all_users_v2())
-print(User(*user))
