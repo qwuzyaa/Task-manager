5db5fcf76e1eccdc49cb4b993ca3352715526fb6
qwuzyaa <nassenova@inbox.ru>
2026-05-19 05:23:36 +0300

    Созданы все шаблоны страниц

--- application/pages.py
+++ application/pages.py
@@ -6,11 +6,7 @@ router = APIRouter(tags=['pages'])
 
 templates = Jinja2Templates(directory='templates')
 
-'''
-@router.get("/startpage", response_class=HTMLResponse)
-def home_page(request: Request):
-    return templates.TemplateResponse("startpage.html", {"request": request})
-'''
+
 @router.get("/", response_class=HTMLResponse)
 def start_page(request: Request):
     return templates.TemplateResponse("startpage.html", {"request": request})
@@ -22,3 +18,11 @@ def home_page(request: Request):
 @router.get("/create", response_class=HTMLResponse)
 def create_task_page(request: Request):
     return templates.TemplateResponse("create.html", {"request": request})
+
+@router.get("/edituser", response_class=HTMLResponse)
+def edit_user_page(request: Request):
+    return templates.TemplateResponse("edituser.html", {"request": request})
+
+@router.get("/edittask", response_class=HTMLResponse)
+def edit_task_page(request: Request):
+    return templates.TemplateResponse("edittask.html", {"request": request})
